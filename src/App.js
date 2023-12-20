9280caa056408b98468a8e9ef670e7d88af7c394
pj-01 <96671928+pj-pj-pj@users.noreply.github.com>
2023-12-20 11:33:35 +0800

    make stats updatable based on items in the list

--- src/App.js
+++ src/App.js
@@ -28,13 +28,13 @@ function App() {
         onDeleteItem={handleDeleteItem}
         onTogglePackedItem={handleTogglePackedItem}
       />
-      <Stats />
+      <Stats items={items} />
     </div>
   );
 }
 
 function Logo() {
-  return <h1>🏮 PACKiFY 🧣</h1>;
+  return <h1>💼 PACKiFY 👜</h1>;
 }
 
 function Form({ onAddItems }) {
@@ -125,10 +125,28 @@ function Item({ item, onDeleteItem, onTogglePackedItem }) {
   );
 }
 
-function Stats() {
+function Stats({ items }) {
+  if (!items.length)
+    return (
+      <p className='stats'>
+        <em>Don't forget to list any items 👜 for your trip!</em>
+      </p>
+    );
+
+  const numItems = items.length;
+  const packedCount = items.filter((item) => item.packed).length;
+  const packedPercent = Math.round((packedCount / numItems) * 100);
+
   return (
     <footer className='stats'>
-      <em>You have x items on your list 🔖, and you already packed x. </em>
+      <em>
+        {packedPercent === 100
+          ? 'You got everything! You are ready to go 🚀.'
+          : `You have ${numItems} ${
+              numItems === 1 ? 'item' : 'items'
+            } 👜 on your list,
+        and you packed ${packedCount} (${packedPercent}%) 🛫.`}
+      </em>
     </footer>
   );
 }
